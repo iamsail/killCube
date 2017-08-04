7237163b041df6880386cda95e184117a9d104d5
iamsail <865605793@qq.com>
2017-08-04 20:14:04 +0800

    增加UglifyJSPlugin插件

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,10 @@ const path = require('path');
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const config = {
     // devtool: 'eval-source-map',//配置生成Source Maps,选择合适的选项
-    devtool: 'false',//生产环境使用,压缩文件体积更小
+    devtool: 'false',//生产环境使用,bundle文件体积更小
     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
         path: path.resolve(__dirname, 'public'),//打包后的文件存放的地方
@@ -60,7 +61,17 @@ const config = {
                 }
             }
         }),
-        new ExtractTextPlugin("styles.css")
+        new ExtractTextPlugin("styles.css"),
+        // new UglifyJSPlugin({
+        //     minimize: true,
+        //     compress:true
+        // })
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false,
+            },
+        }),
     ]
 };
 
